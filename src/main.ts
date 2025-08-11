4d718151415b4c436489d8e4330934ddf27c5418
Wiliyanto <wiliyanto@grip-principle.com>
2025-08-11 07:02:48 +0700

    feat: Add swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (process.env.ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Task API')
+      .setDescription('The task API description')
+      .setVersion('1.0')
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+  }
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
